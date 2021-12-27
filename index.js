e0cf736214ab586a8db2ccf0918b88ea9953fe4a
jieun <jieun.p974@gmail.com>
2021-12-27 19:37:57 +0900

    bodyParser, nodemon

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const express = require('express')
 const app = express()
 //5000번 포트를 사용함
 const port = 5000
+const bodyParser = require('body-parser')
+// user모델 가져오기
+const { User } = require('./models/User')
+
+// application/x-www-form-urlencoded 타입을 가져올 수 있음
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json 타입을 가져올 수 있음
+app.use(bodyParser.json());
+
+
 //mongoDB 연결
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://dbUser:1234@cluster0.xuhf2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
@@ -12,10 +22,25 @@ mongoose.connect('mongodb+srv://dbUser:1234@cluster0.xuhf2.mongodb.net/myFirstDa
 }).then(() => console.log('MongoDB Connected..'))
   .catch(err => console.log(err))
 
-
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!2022')
+  })
+
+app.post('/register',(req, res) => {
+  //회원 가입 할때 필요한 목록들을 client에서 가져오면
+  //그것들을 DB에 넣어준다.
+
+  const user = new User(req.body)
+  // mongoDB에서 오는 method
+  user.save((err, userInfo)=>{
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
